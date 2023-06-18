1f6280d0bf53d31198ad2151b50921540c6901e4
saltysaff <d_ash12345@hotmail.co.uk>
2023-06-18 17:10:38 +0100

    fix minor ui bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Intro from "./Intro.jsx";
 import Question from "./Question.jsx";
 import { nanoid } from "nanoid";
+import { decode } from "html-entities";
 
 export default function App() {
   const [questions, setQuestions] = useState([]);
@@ -11,32 +12,33 @@ export default function App() {
     fetch("https://opentdb.com/api.php?amount=5")
       .then((res) => res.json())
       .then((questionsRes) => {
-        setQuestions(questionsRes.results.map((question) => formatQuestion(question)));
+        setQuestions(
+          questionsRes.results.map((question) => formatQuestion(question))
+        );
       });
   }
 
   function formatQuestion(question) {
     return {
       id: nanoid(),
-      question: question.question,
-      possibleAnswers: shuffleArray([
-        ...question.incorrect_answers,
-        question.correct_answer,
-      ]),
-      correct_answer: question.correct_answer,
+      question: decode(question.question),
+      possibleAnswers: shuffleArray(
+        decodeArray([...question.incorrect_answers, question.correct_answer])
+      ),
+      correct_answer: decode(question.correct_answer),
     };
   }
 
-  const questionElements = questions.map(question => (
-    <Question 
+  const questionElements = questions.map((question) => (
+    <Question
       key={question.id}
       question={question.question}
       correctAnswer={question.correct_answer}
       possibleAnswers={question.possibleAnswers}
     />
-  ))
+  ));
 
-  console.log(questionElements)
+  console.log(questionElements);
 
   function shuffleArray(array) {
     let currentIndex = array.length,
@@ -52,6 +54,10 @@ export default function App() {
     return array;
   }
 
+  function decodeArray(array) {
+    return array.map((element) => decode(element));
+  }
+
   useEffect(() => {
     console.log(questions);
   }, [questions]);
@@ -59,11 +65,12 @@ export default function App() {
   return (
     <main>
       {questions.length === 0 && <Intro getQuestions={getQuestions} />}
-      {questions.length > 0 && (
-        <div>
-          {questionElements}
-        </div>
-      )}
+      {questions.length > 0 && 
+      <div className="question-form-container">
+        {questionElements}
+        <button className="check-answers">Check Answers</button>
+      </div>
+      }
     </main>
   );
 }
